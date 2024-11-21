3af6c2ce634a7334ad60f6c2a4482c53c5fd0161
David <1716282441@qq.com>
2024-11-21 15:43:00 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2,52 +2,45 @@ function setup() {
   createCanvas(1450, 700);
 }
 
- function preload() {
-   img = loadImage('character.png'); // Ensure the image is uploaded in the editor
- }
-
- function hero(x,y,rotation){
- push();
- translate(x,y);
- rotate(rotation);
- image(img,-50, -50, 100, 100); // Display the image at (50, 50) with a size of 300x200
- fill(0);
- ellipse(43,24,3);
- pop();
-
+function preload() {
+  img = loadImage("character.png"); // Ensure the image is uploaded in the editor
 }
 
- let img;
- let x = 50;
- let y = 50;
- let rotation = 0;
- let speed = 0;
- let bullets = [];
+function hero(x, y, rotation) {
+  push();
+  translate(x, y);
+  rotate(rotation);
+  image(img, -50, -50, 100, 100); // Display the image at (50, 50) with a size of 300x200
+  fill(0);
+  ellipse(43, 24, 3);
+  pop();
+}
 
- function draw() {
-   background(150);
-   hero (x,y,rotation);
+let img;
+let x = 50;
+let y = 50;
+let rotation = 0;
+let speed = 0;
+let bullets = [];
 
-   x = x + Math.cos(rotation)*speed;
-   y = y + Math.sin(rotation)*speed;
-   
-   if(keyIsDown(38)){
+function draw() {
+  background(150);
+  hero(x, y, rotation);
+
+  x = x + Math.cos(rotation) * speed;
+  y = y + Math.sin(rotation) * speed;
+
+  if (keyIsDown(38)) {
     speed = 5;
-  }
-  
-  else if (keyIsDown(40)){
-   speed =-5;
-  }
-
-  else{
-    speed =0;
+  } else if (keyIsDown(40)) {
+    speed = -5;
+  } else {
+    speed = 0;
   }
 
-  if(keyIsDown(37)){
-  rotation = rotation - 0.05;
-  }
-
-  else if(keyIsDown(39)){
+  if (keyIsDown(37)) {
+    rotation = rotation - 0.05;
+  } else if (keyIsDown(39)) {
     rotation = rotation + 0.05;
   }
 
@@ -65,7 +58,6 @@ function setup() {
       bullets.splice(i, 1); // Remove the bullet
     }
   }
-
 }
 
 function mousePressed() {
@@ -83,13 +75,3 @@ function mousePressed() {
   };
   bullets.push(bullet); // Add the bullet to the array
 }
- 
-
-
- 
-  
-
-  
-
-
- 
