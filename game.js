75b11542675611b1d861d14093ee8483552c37c3
Binu993 <sebastian.binu993@gmail.com>
2024-12-03 02:35:46 +0530

    enemy shooting speed updated

--- game.js
+++ game.js
@@ -160,7 +160,7 @@ class NPC {
     this.speed = random(0.5, 1.5);
     this.moveCooldown = floor(random(60, 180));
     this.timer = 0;
-    this.shootCooldown = floor(random(120, 180)); // Shooting cooldown
+    this.shootCooldown = floor(random(30, 50)); // Shooting cooldown
     this.shootTimer = 0;
 
     this.currentRotation = random(TWO_PI); // 当前旋转角度
