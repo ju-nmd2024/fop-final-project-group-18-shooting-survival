2604807c4fefc289489d3665c2760b324c59b005
David <1716282441@qq.com>
2024-12-05 13:37:02 +0100

    Update game.js

--- game.js
+++ game.js
@@ -44,7 +44,7 @@ function createParticles(x, y) {
 function preload() {
   character = loadImage("img/character.png");
   enemy = loadImage("img/enemy2.png");
-  gameStart = loadImage("img/game start.png");
+  gameStart = loadImage("img/game start final.png");
   winBackground = loadImage("img/win game.webp");
   lostBackground = loadImage("img/fault.webp");
   backgroundImage = loadImage("img/R.jpg");
